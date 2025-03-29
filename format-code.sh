b87d6ca1909b071178185a05cbf6aa120d13908e
Justin Garrigus <justin.m.garrigus@gmail.com>
2025-03-29 02:45:03 +0000

    Added all files from original GPGPU-Sim

---
+++ format-code.sh
@@ -0,0 +1,13 @@
+# This bash script formats GPGPU-Sim using clang-format
+THIS_DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+echo "Running clang-format on $THIS_DIR"
+clang-format -i ${THIS_DIR}/libcuda/*.h
+clang-format -i ${THIS_DIR}/libcuda/*.cc
+clang-format -i ${THIS_DIR}/src/*.h
+clang-format -i ${THIS_DIR}/src/*.cc
+clang-format -i ${THIS_DIR}/src/gpgpu-sim/*.h
+clang-format -i ${THIS_DIR}/src/gpgpu-sim/*.cc
+clang-format -i ${THIS_DIR}/src/cuda-sim/*.h
+clang-format -i ${THIS_DIR}/src/cuda-sim/*.cc
+clang-format -i ${THIS_DIR}/src/accelwattch/*.h
+clang-format -i ${THIS_DIR}/src/accelwattch/*.cc
